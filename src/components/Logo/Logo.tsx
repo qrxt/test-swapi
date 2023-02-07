68b378334f27602ab8599e8c67f9f3a2be241f8d
qrx <qrx357@gmail.com>
2023-02-08 01:46:29 +0300

    Make logo clickable

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import logoImage from "assets/images/logo.png";
+import { Link, useLocation } from "react-router-dom";
+
+function LogoImage() {
+  return <img src={logoImage} width="150" height="80" alt="Application logo" />;
+}
 
 function Logo() {
-  return <img src={logoImage} width="150" height="80" alt="Application logo" />;
+  const { pathname } = useLocation();
+
+  return pathname === "/" ? (
+    <LogoImage />
+  ) : (
+    <Link to="/">
+      <LogoImage />
+    </Link>
+  );
 }
 
 export default Logo;
